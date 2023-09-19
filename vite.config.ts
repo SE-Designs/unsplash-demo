c0f87e60797d6846b972b939eae1640c42ee99d0
mixture <dragonlorefnstattrack@gmail.com>
2023-09-19 20:26:47 +0600

    UPD: register unplugins

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,53 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      imports: ['vue'],
+      dts: 'src/auto-imports.d.ts'
+    }),
+    Components({
+      // allow auto load markdown components under `./src/components/`
+      extensions: ['vue', 'md'],
+
+      // allow auto import and register components used in markdown
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+
+      // custom resolvers
+      resolvers: [
+        // auto import icons
+        // https://github.com/antfu/unplugin-icons
+      ],
+
+      dts: 'src/components.d.ts'
+    })
   ],
+  server: {
+    port: 3000
+  },
+  preview: {
+    port: 3000
+  },
+  mode: 'development',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+        @import "@/assets/main.scss";
+        `
+      }
+    }
+    // devSourcemap: true
   }
 })
