6a7e079aabfafb40ecf6c4098dea3112acf5791c
mixture <dragonlorefnstattrack@gmail.com>
2023-09-20 02:54:12 +0600

    UPD: favorite view

--- src/views/FavouriteView.vue
+++ src/views/FavouriteView.vue
@@ -1,7 +1,77 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import DeleteIcon from '@/assets/icons/DeleteIcon.vue'
+import EyeIcon from '@/assets/icons/EyeIcon.vue'
+import { getFavorites, removeFavorite } from '@/utils/useStorage'
+
+const favorites = ref(getFavorites())
+
+function handleRemoveFavorite(id: string) {
+  removeFavorite(id)
+  favorites.value = getFavorites()
+}
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <main>
+    <h2>Избранное</h2>
+    <div class="container">
+      <div class="grid" v-if="favorites.length > 0">
+        <RouterLink
+          class="photo-link"
+          v-for="item in favorites"
+          :key="item.id"
+          :to="{ name: 'Photo', params: { id: item.id } }"
+        >
+          <div class="remove" @click.prevent="handleRemoveFavorite(item.id)">
+            <DeleteIcon />
+          </div>
+          <img :src="item.photo" width="300" height="300" />
+        </RouterLink>
+      </div>
+      <div class="nothing-container">
+        <div class="icon-wrapper">
+          <EyeIcon />
+        </div>
+        <h3>Здесь еще ничего нет, добавьте свою первую фотографию :D</h3>
+      </div>
+    </div>
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.remove {
+  position: absolute;
+  top: 12px;
+  right: 12px;
+
+  width: 40px;
+  height: 40px;
+
+  color: #0e4e9a;
+  background-color: $white;
+  border-radius: 6px;
+  border: 1px solid $gray;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  z-index: 15;
+}
+
+.nothing-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  & .icon-wrapper {
+    font-size: 42px;
+  }
+
+  & h3 {
+    font-size: 24px;
+  }
+}
+</style>
