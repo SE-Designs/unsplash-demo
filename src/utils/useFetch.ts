d1925d52debd4c9c8cca4387cbe906f17bd99e39
mixture <dragonlorefnstattrack@gmail.com>
2023-09-19 23:35:07 +0600

    ADD: use fetch composable

---
+++ src/utils/useFetch.ts
@@ -0,0 +1,26 @@
+import { ref, watchEffect, toValue } from 'vue'
+
+export function useFetch(url: string) {
+  const data = ref()
+  const error = ref()
+  const loading = ref(false)
+
+  async function fetchData() {
+    try {
+      loading.value = true
+      const response = await fetch(url)
+      const json = await response.json()
+      data.value = json
+    } catch (err) {
+      error.value = err
+    } finally {
+      loading.value = false
+    }
+  }
+
+  watchEffect(() => {
+    fetchData()
+  })
+
+  return { data, loading, error }
+}
