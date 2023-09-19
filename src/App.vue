2e83f38fd3810f19807d299de7c2357cc0a6829e
mixture <dragonlorefnstattrack@gmail.com>
2023-09-19 20:16:58 +0600

    ADD: init scaffolding and setup

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+
+console.log(import.meta.env.VITE_API_URL)
+</script>
+
+<template>
+  <NavHeader />
+  <RouterView />
+</template>
+
+<style lang="scss">
+@import './assets/base.scss';
+</style>
---
+++ src/components/NavHeader.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <nav></nav>
+  </header>
+</template>
+
+<style scoped lang="scss">
+header {
+  height: 80px;
+  width: 100vw;
+  background-color: $black;
+}
+
+nav {
+  display: flex;
+  // flex-direction: row;
+  // flex-wrap: nowrap;
+
+  align-items: center;
+  justify-content: center;
+}
+</style>
